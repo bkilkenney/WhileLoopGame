5dcfd3e80e7f9c69f5f5c9abdd9633a3ce173180
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-14 00:13:51 -0400

    Something to turn in

--- YouWon2/Program.cs
+++ YouWon2/Program.cs
@@ -10,6 +10,25 @@ namespace YouWon2
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            Console.WriteLine("Pick an integer.");
+            int usersAnswer = int.Parse(Console.ReadLine());
+            
+            for (i = 0; i < int.MaxValue; i++)
+            {
+                for(i = 0; i % 3 !=0; i++)
+
+
+                
+                {
+                    Console.WriteLine("You lost.");
+                }
+
+
+                Console.WriteLine("You won.");
+                break;
+       
+            }
         }
     }
 }
